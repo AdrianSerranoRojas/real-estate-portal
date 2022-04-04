67a6a10bb76e6d32cc370aeb408e523e4ce96a0d
Adrián Serrano <adrianno_3@hotmail.com>
2022-04-04 15:57:46 +0200

    React files

---
+++ FRONTEND/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
+import './App.css'
+
+import Dashboard from './Pages/Dashboard';
+import Home from './Pages/Home';
+
+function App() {
+
+
+  return (
+<>
+<Routes>
+  <Route path="/" element={<Home />}/>
+  <Route path="/dashboard" element={<Dashboard />}/>
+
+  <Route path="*" element={<Home />}/>
+</Routes>
+
+
+</>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import withLayout from '../../hoc/withLayout'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default withLayout(Dashboard)
